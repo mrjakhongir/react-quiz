c99ddfacbe75d22a8d4252b9f91d3ece6b957b7c
Jahongir <nusratovjahongir@gmail.com>
2023-08-09 10:35:20 +0500

    build and deploy react-quiz app

---
+++ src/components/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
+import { AnswerObject } from "../App";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestions,
+}) => {
+  return (
+    <Wrapper>
+      <p className="number">
+        Question: {questionNumber} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers?.map((answer, index) => (
+          <ButtonWrapper
+            key={index}
+            correct={userAnswer?.correctAnswer === answer}
+            userClicked={userAnswer?.answer === answer}
+          >
+            <button disabled={!!userAnswer} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+            </button>
+          </ButtonWrapper>
+        ))}
+      </div>
+    </Wrapper>
+  );
+};
+
+export default QuestionCard;
